ee1cc400d09d59d24fe13fbf091978e9f26d0ef2
Ritik Ranjan Gupta <ritikranjangupta@ritik.gupta-MacBookPro>
2023-02-11 17:55:18 +0530

    added request

--- crates/router/src/connector/swedbank/transformers.rs
+++ crates/router/src/connector/swedbank/transformers.rs
@@ -3,7 +3,10 @@ use crate::{core::errors,types::{self,api, storage::enums}};
 
 //TODO: Fill the struct with respective fields
 #[derive(Default, Debug, Serialize, Eq, PartialEq)]
-pub struct SwedbankPaymentsRequest {x}
+#[serde(rename_all = "camelCase")]
+pub struct SwedbankPaymentsRequest {
+    payment: Payment,
+}
 
 impl TryFrom<&types::PaymentsAuthorizeRouterData> for SwedbankPaymentsRequest  {
     type Error = error_stack::Report<errors::ConnectorError>;
@@ -135,3 +138,107 @@ pub struct SwedbankErrorResponse {
     pub detailed_error_code: String,
     pub detailed_error_description: String,
 }
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Payment {
+    operation: String,
+    intent: String,
+    currency: String,
+    prices: Prices,
+    description: String,
+    user_agent: String,
+    language: String,
+    urls: Urls,
+    payee_info: PayeeInfo,
+    payer: Payer,
+    cardholder: Cardholder,
+    risk_indicator: RiskIndicator
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Prices {
+    #[serde(rename = "type")]
+    type_of_response: String,
+    amount: i64,
+    vat_amount: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Urls {
+    complete_url: String,
+    cancel_url: String,
+    callback_url:String,
+    logo_url: String,
+    terms_of_service_url: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PayeeInfo {
+    payee_id: String,
+    payee_reference: String,
+    payee_name: String,
+    product_category: String,
+    order_reference: String,
+    subsite: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Payer {
+    payer_reference: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Cardholder {
+    first_name: String,
+    last_name: String,
+    email: String,
+    msisdn: String,
+    home_phone_number: String,
+    work_phone_number: String,
+    shipping_address: ShippingAddress,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ShippingAddress {
+    first_name: String,
+    last_name: String,
+    email: String,
+    msisdn: String,
+    street_address: String,
+    co_address: String,
+    city: String,
+    zip_code: String,
+    country_code: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RiskIndicator {
+    delivery_email_address: String,
+    delivery_time_frame_indicator: String,
+    pre_order_date: String,
+    pre_order_purchase_indicator: String,
+    ship_indicator: String,
+    gift_card_purchase: bool,
+    re_order_purchase_indicator: String,
+    pick_up_address: PickUpAddress
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PickUpAddress {
+    name: String,
+    street_address: String,
+    co_address: String,
+    city: String,
+    zip_code: String,
+    country_code: String,
+}
+
